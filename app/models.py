9b76782abe106c12cf28914553adcf641bc26fac
Priscilla Odame <42655518+Priscilla-Odame@users.noreply.github.com>
2022-09-26 21:39:37 +0000

    write functionality (#1)
    * implement event api;model, view and url
    
    * add unit test

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class EventLog(models.Model):
+    event_id = models.CharField(max_length=300, unique=True)
+    event_type = models.CharField(max_length=600)
+    timestamp = models.DateTimeField(auto_now=True)
+    event_data = models.JSONField(default = dict)
